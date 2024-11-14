0fd62ac29cc9e34ba7b95c78448ef869a28244d9
Manuel Montero <87241159+ManuelMC21@users.noreply.github.com>
2024-11-14 18:58:35 +0100

    Proyecto BackEnd Iniciado
    Conectando la base de Datos

---
+++ Mapeo/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Mapeo.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
---
+++ Mapeo/Models/Restaurant.cs
@@ -0,0 +1,8 @@
+public class Restaurant{
+
+    public int Id { get; set; }
+
+    public string? Name { get; set; }
+
+    public string? Phone { get; set; }
+}
