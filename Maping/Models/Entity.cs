ec16064d8cc6c5d4b60914edb1af87d6b1ebfc58
Manuel Montero <87241159+ManuelMC21@users.noreply.github.com>
2024-12-10 21:41:48 +0100

    FirstCommit
    Proyecto BackEnd más completo y estable

---
+++ Maping/Models/Entity.cs
@@ -0,0 +1,17 @@
+using NetTopologySuite.Geometries;
+
+public class Entity
+{
+    public int Id { get; set; }
+    public Point Geom { get; set; }
+
+    public string UserId { get; set; }
+    public int DistrictId { get; set; }
+    public int EntityTypeId { get; set; }
+
+    //Navigation Variables
+    public ApplicationUser user { get; set; }
+    public EntityType EntityType { get; set; }
+    public District District { get; set; }
+    public Restaurant Restaurant { get; set; }
+}
